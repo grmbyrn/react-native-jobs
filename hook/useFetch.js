a24f615ca832a4f247372ecc32a8cc149dc2f757
grmbyrn <graemebyrne_89@hotmail.com>
2023-04-22 13:36:40 +0200

    add search functionality, connect to rapidapi, add nearby jobs and popular jobs data

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -1,12 +1,11 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-const useFetch = (endpoint) => {
+
+const useFetch = (endpoint, query) => {
     const [data, setData] = useState([])
     const [isLoading, setIsLoading] = useState(false)
     const [error, setError] = useState(null)
-
-    const axios = require("axios");
 
     const options = {
         method: 'GET',
@@ -15,6 +14,35 @@ const useFetch = (endpoint) => {
             'X-RapidAPI-Key': '',
             'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
         },
-        params: {query: 'Python developer in Texas, USA', page: '1', num_pages: '1'},
+        params: {...query},
     };
+
+    const fetchData = async () => {
+        setIsLoading(true)
+
+        try {
+            const response = await axios.request(options)
+
+            setData(response.data.data)
+            setIsLoading(false)
+        } catch (error){
+            setError(error)
+            alert('There is an error')
+        } finally {
+            setIsLoading(false)
+        }
+    }
+
+    useEffect(() => {
+        fetchData()
+    }, [])
+
+    const refetch = () => {
+        setIsLoading(true)
+        fetchData()
+    }
+
+    return {data, isLoading, error, refetch}
 }
+
+export default useFetch
